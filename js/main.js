4efcb3b055b60bfd41cfac8d97e4bcdc330a8d61
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-05-30 22:37:10 +0500

    offer-card page the end

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let header_burger = document.querySelector('.header_burger');
 let bg = document.querySelector('.bg');
 let media_aside = document.querySelector('.media_aside');
+let media_aside_close = document.querySelector('.media_aside_close');
 
 header_burger.addEventListener('click', () => {
     bg.classList.add('active');
@@ -8,6 +9,11 @@ header_burger.addEventListener('click', () => {
 });
 
 bg.addEventListener('click', () => {
+    bg.classList.remove('active');
+    media_aside.classList.remove('active');
+});
+
+media_aside_close.addEventListener('click', () => {
     bg.classList.remove('active');
     media_aside.classList.remove('active');
 });

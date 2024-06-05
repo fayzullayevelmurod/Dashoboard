ba5ef68dcf4621d1f0a4b63e2f5e469ed7efbf06
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-06-05 16:24:50 +0500

    settings-referal start

--- js/main.js
+++ js/main.js
@@ -17,3 +17,29 @@ media_aside_close.addEventListener('click', () => {
     bg.classList.remove('active');
     media_aside.classList.remove('active');
 });
+
+
+// select
+let index = 1;
+
+const on = (listener, query, fn) => {
+	document.querySelectorAll(query).forEach(item => {
+		item.addEventListener(listener, el => {
+			fn(el);
+		})
+	})
+}
+
+on('click', '.selectBtn', item => {
+	const next = item.target.nextElementSibling;
+	next.classList.toggle('toggle');
+	next.style.zIndex = index++;
+});
+on('click', '.option', item => {
+	item.target.parentElement.classList.remove('toggle');
+
+	const parent = item.target.closest('.select').children[0];
+	parent.setAttribute('data-type', item.target.getAttribute('data-type'));
+	parent.innerText = item.target.innerText;
+})
+// select
